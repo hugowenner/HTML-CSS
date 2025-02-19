c74f576567aef72cbdf4a183a1b9d7900dff6541
Hugo Wenner <113842460+hugowenner@users.noreply.github.com>
2025-02-19 20:51:17 -0300

    Add files via upload

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,16 @@
 alert('Bem vindo ao nosso GAME!');
-let numeroSecreto = 29;
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+
 let chute = prompt('Escolha um número entre 1 e 30');
 
-if (numeroSecreto == chute) {
+if (chute == numeroSecreto) {
     console.log('Isso ai, voce descobriu o codigo secreto,Parabens!!!!!');
+    alert(`Isso ai, voce descobriu o codigo secreto,Parabens!!!!! ${numeroSecreto}`
+        )
 }
+
+else {
+    alert('Errou!!!')
+}
+
