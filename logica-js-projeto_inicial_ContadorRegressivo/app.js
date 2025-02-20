ea19d7b0077d793407f77346bd89003993512216
Hugo Wenner <113842460+hugowenner@users.noreply.github.com>
2025-02-20 07:52:44 -0300

    Add files via upload
    Contador regressivo feito em JAVASCRIPT

---
+++ logica-js-projeto_inicial_ContadorRegressivo/app.js
@@ -0,0 +1,26 @@
+// Desafio 1
+let ContadorCrescente = 1;
+while (ContadorCrescente <=10) {
+    alert (` Número ${ContadorCrescente}`);
+    ContadorCrescente++
+}
+// Desafio 2
+let ContadorDecrescente = 10;
+while (ContadorDecrescente >=0) {
+    alert (` Número ${ContadorDecrescente}`);
+        ContadorDecrescente--
+}
+// Desafio 3
+let ContagemDecrescente = prompt('Digite um número de 01 ao 10');
+while (ContagemDecrescente >= 0) {
+    alert (`${ContagemDecrescente}`);
+    ContagemDecrescente-- 
+}
+// Desafio 4
+let ContagemCrescente = prompt('Digite um número de 1 ao 10');
+let Numero = 0;
+while (Numero <= ContagemCrescente) {
+    alert (`${Numero}`);
+    Numero++
+}
+
